3a9d476380bd2ad083df70493d4c10eb5ae62aa4
sohyunpark-physics <sp13328@bristol.ac.uk>
2025-01-23 12:49:35 +0000

    leaving home

--- C++/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    int file_size = 100;
-    double sales = 9.99;
-    std::cout << file_size; // cout: character out
-    return 0; // 0 tells operating system that the program will terminate correctly
-}
